555601e76b835ed57e5e02f455007301f107fc57
sankarsans <sssankarsam@gmail.com>
2021-08-31 18:38:52 +0530

    contact form and quote form updated

---
+++ enquiry.php
@@ -0,0 +1,35 @@
+<?php
+if ( ($_POST['name']!="") && ($_POST['email']!="")){
+$name = $_POST['name'];
+$phonenumber = $_POST['phone_number'];
+// $companyname = $_POST['companyname'];
+$email = $_POST['email'];
+$business = $_POST['trialbusiness'];
+
+$to = "Ken@kenjacksoninsurance.com";
+$subject = "kenjacksoninsurance Contact Form";
+$fromname = 'Ken@kenjacksoninsurance.com';
+$message = "<p>New Email is Received: </p>  
+<p>First Name : $name.</p> 
+<p>Email : $email.</p> 
+<p>Phone Number : $phonenumber.</p> 
+<p>Insurance Type : $business.</p>"; 
+
+ 
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+$headers .= "From: <".$fromname.">" . "\r\n";
+$sent = mail($to,$subject,$message,$headers);
+if($sent){
+ echo "<span style='color:green; font-weight:normal;'>
+ Thank you for contacting us, we will get back to you shortly.
+ </span>";
+}
+else{
+ echo "<span style='color:red; font-weight:normal;'>
+ Sorry! Your form submission is failed.
+ </span>";
+ }
+}
+?>
